398a1e0e66cdf23d636767e8c676cc9736ad857b
lando365 <149484349+lando365@users.noreply.github.com>
2025-06-19 19:58:31 +0200

    route

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Le chemin vers la route "accueil" de votre application.
+     *
+     * Typiquement, les utilisateurs sont redirigés ici après authentification.
+     *
+     * @var string
+     */
+    public const HOME = '/dashboard';
+
+    /**
+     * Définissez vos liaisons de modèle de route, filtres de modèle et autre configuration de route.
+     */
+    public function boot(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
+    }
+}
